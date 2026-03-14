6b66ed0ee814837093a1cc8eeada9c1238a7c45e
Sukanta Dutta <sukantadutta2003@gmail.com>
2026-03-14 15:15:04 +0530

    fix vite define global bug causing blank screen

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: 'globalThis',   // sockjs-client uses Node's `global`, shim it for browser
-  },
 })
